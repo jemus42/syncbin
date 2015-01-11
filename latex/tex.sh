527d91cd695c13d56a0972cb5ab4196449fafc5c
jemus42 <jemus42@quantenbrot.de>
2015-01-11 23:32:18 +0100

    Formatting

--- latex/tex.sh
+++ latex/tex.sh
@@ -5,16 +5,17 @@
 echo "1) Processing $1.tex with pdflatex (x2)…";
 /usr/texbin/pdflatex -synctex=1 -interaction=nonstopmode $1.tex > /dev/null;
 /usr/texbin/pdflatex -synctex=1 -interaction=nonstopmode $1.tex > /dev/null; 
-echo "Done.\n2) Starting bibtex processing… (bibtex - pdflatex - bibtex)";
+echo "Done."
+echo "2) Starting bibtex processing… (bibtex - pdflatex - bibtex)";
 /usr/texbin/bibtex $1.aux > /dev/null;
 /usr/texbin/pdflatex -synctex=1 -interaction=nonstopmode $1.tex > /dev/null;
 /usr/texbin/bibtex $1.aux > /dev/null;
-
-echo "Done.\n3) Starting second processing with pdflatex (x2)…";
+echo "Done."
+echo "3) Starting second processing with pdflatex (x2)…";
 /usr/texbin/pdflatex -synctex=1 -interaction=nonstopmode $1.tex > /dev/null;
 /usr/texbin/pdflatex -synctex=1 -interaction=nonstopmode $1.tex > /dev/null;
-echo "Done processing, removing garbage… (.aux, .synctex.gz, .out"
-rm *.aux *.synctex.gz *.out;
+echo "Done processing, removing garbage… (.aux, .synctex.gz, .out)"
+rm *.aux *.synctex.gz *.out *.blg *.toc *.bbl *.log;
 echo "4) Pushing to filedump";
 scp $1.pdf strato:"~/dumpdir/Hausarbeiten/";
 #echo "Done.\nOpening preview for $1.pdf in Quick Look if specified (TBI)...";

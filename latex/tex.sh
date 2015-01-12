795ab9d6af6b8dcd7863fdbbb7d37d5b8bfcd116
jemus42 <jemus42@quantenbrot.de>
2015-01-12 13:06:44 +0100

    Formatting

--- latex/tex.sh
+++ latex/tex.sh
@@ -14,11 +14,10 @@ echo "Done."
 echo "3) Starting second processing with pdflatex (x2)…";
 /usr/texbin/pdflatex -synctex=1 -interaction=nonstopmode $1.tex > /dev/null;
 /usr/texbin/pdflatex -synctex=1 -interaction=nonstopmode $1.tex > /dev/null;
-echo "Done processing, removing garbage… (.aux, .synctex.gz, .out)"
+echo "Done processing, removing garbage… (.aux, .synctex.gz, .out, …)"
 rm *.aux *.synctex.gz *.out *.blg *.toc *.bbl *.log;
 echo "4) Pushing to filedump";
 scp $1.pdf strato:"~/dumpdir/Hausarbeiten/";
 #echo "Done.\nOpening preview for $1.pdf in Quick Look if specified (TBI)...";
-#qlmanage -p $1.pdf > /dev/null &;
+#qlmanage -p $1.pdf > /dev/null;
 echo "All done.";
-exit

18c2f081b678744ad27acb2b5dd87ee5f8790138
Lukas Burk <lukas@quantenbrot.de>
2019-03-13 21:06:04 +0100

    MOAR

--- zsh/link.sh
+++ zsh/link.sh
@@ -4,3 +4,4 @@
 ln -sf $SYNCBIN/zsh/zshrc $HOME/.zshrc
 ln -fs $SYNCBIN/zsh/jemus42.zsh-theme ~/.oh-my-zsh/themes/jemus42.zsh-theme
 ln -sf $SYNCBIN/zsh/liquidpromptrc $HOME/.config/liquidpromptrc
+ln -sf $SYNCBIN/screenrc $HOME/.screenrc

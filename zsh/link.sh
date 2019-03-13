2716f9eee15cf15d8dc78590a466f8c687616888
Lukas Burk <lukas@quantenbrot.de>
2019-03-13 21:19:06 +0100

    sigh

--- zsh/link.sh
+++ zsh/link.sh
@@ -3,6 +3,6 @@
 SYNCBIN=$HOME/syncbin
 
 ln -sf $SYNCBIN/zsh/zshrc $HOME/.zshrc
-ln -fs $SYNCBIN/zsh/jemus42.zsh-theme ~/.oh-my-zsh/themes/jemus42.zsh-theme
+ln -sf $SYNCBIN/zsh/jemus42.zsh-theme $HOME/.oh-my-zsh/themes/jemus42.zsh-theme
 ln -sf $SYNCBIN/zsh/liquidpromptrc $HOME/.config/liquidpromptrc
 ln -sf $SYNCBIN/screenrc $HOME/.screenrc

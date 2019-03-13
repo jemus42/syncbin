5183b016a7c90e446ed0a59439dd45d2a49ffe93
Lukas Burk <lukas@quantenbrot.de>
2019-03-13 20:49:31 +0100

    I should clean this stuff up more often

--- zsh/link.sh
+++ zsh/link.sh
@@ -1,2 +1,6 @@
 #! /bin/sh
+
+
+ln -sf $SYNCBIN/zsh/zshrc $HOME/.zshrc
 ln -fs $SYNCBIN/zsh/jemus42.zsh-theme ~/.oh-my-zsh/themes/jemus42.zsh-theme
+ln -sf $SYNCBIN/zsh/liquidpromptrc $HOME/.config/liquidpromptrc

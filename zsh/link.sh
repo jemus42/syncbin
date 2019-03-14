e39ea378b5c741680cb967954cb01642bee323a7
Lukas Burk <lukas@quantenbrot.de>
2019-03-14 18:28:23 +0100

    All the cleanup

--- zsh/link.sh
+++ zsh/link.sh
@@ -1,8 +1,35 @@
 #! /usr/bin/env zsh
 
-SYNCBIN=$HOME/syncbin
+# Define SYNCBIN here as well as in zshrc because this might be called on fresh installs
+export SYNCBIN=$HOME/syncbin
 
+# Create .config dir if needed
+if [ -d "$HOME/.config" ]; then mkdir $HOME/.config; fi
+
+# Link stuff into place
 ln -sf $SYNCBIN/zsh/zshrc $HOME/.zshrc
-ln -sf $SYNCBIN/zsh/jemus42.zsh-theme $HOME/.oh-my-zsh/themes/jemus42.zsh-theme
-ln -sf $SYNCBIN/zsh/liquidpromptrc $HOME/.config/liquidpromptrc
 ln -sf $SYNCBIN/screenrc $HOME/.screenrc
+ln -sf $SYNCBIN/zsh/liquidpromptrc $HOME/.config/liquidpromptrc
+
+# Link ZSH theme to OMZSH custom theme dir
+if [ -d "$HOME/.oh-my-zsh" ]; then
+    ln -sf $SYNCBIN/zsh/jemus42.zsh-theme ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/jemus42.zsh-theme
+fi
+
+### Install other plugins if not already present
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ] ; then
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting;
+fi
+
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ] ; then
+    git clone https://github.com/zsh-users/zsh-autosuggestions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions;
+fi
+
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions ] ; then
+    git clone https://github.com/zsh-users/zsh-completions.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions;
+fi
+
+
+if [ ! -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/liquidprompt ] ; then
+    git clone https://github.com/nojhan/liquidprompt.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/liquidprompt;
+fi

a9f888702eeccbab4055b99f50baddc976015a51
Lukas Burk <lukas@quantenbrot.de>
2019-03-15 01:15:47 +0100

    Let the pl9k themeing begin

--- zsh/theme/powerlevel9k-env.sh
+++ zsh/theme/powerlevel9k-env.sh
@@ -2,3 +2,7 @@ POWERLEVEL9K_MODE="nerdfont-complete"
 
 # Hide false-positive(?) warning on FreeBSD
 POWERLEVEL9K_IGNORE_TERM_COLORS=true
+
+
+# Segments
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs anconda virtualenv)

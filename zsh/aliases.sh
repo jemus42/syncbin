fdc5baf4ad1b677084527a94970bbe464e3b9396
Lukas Burk <lukas@quantenbrot.de>
2020-04-09 23:38:38 +0200

    Update stuff

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -48,12 +48,15 @@ alias yt-chronological="youtube-dl -i -o '%(upload_date)s -  %(title)s [%(id)s].
 # If GNU ls is there, make ls be gls
 (( $+commands[gls] )) && alias ls='gls --color=tty'
 
+# ...but if lsd is there, use that
+(( $+commands[lsd] )) && alias ls='lsd'
+
 # ls, the common ones I use a lot shortened for rapid fire usage
-alias l='ls -lFh'     #size,show type,human readable
-alias la='ls -lAFh'   #long list,show almost all,show type,human readable
-alias lr='ls -tRFh'   #sorted by date,recursive,show type,human readable
-alias lt='ls -ltFh'   #long list,sorted by date,show type,human readable
-alias ll='ls -l'      #long list
+alias l='ls -lFh'     # size,show type,human readable
+alias la='ls -lAFh'   # long list,show almost all,show type,human readable
+alias lr='ls -tRFh'   # sorted by date,recursive,show type,human readable
+alias lt='ls -ltFh'   # long list,sorted by date,show type,human readable
+alias ll='ls -l'      # long list
 alias ldot='ls -ld .*'
 alias lS='ls -1FSsh'
 alias lart='ls -1Fcart'

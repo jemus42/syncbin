6af9f7bb99b318a7a2c7641ae443df69f8e6dd65
Lukas Burk <lukas@quantenbrot.de>
2020-04-22 17:20:54 +0200

    Backup iterm2 settings as xml

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -105,7 +105,8 @@ function upall() {
     echo ""
     echo "#########################"
     echo "Backing up iterm2 config"
-    cp $HOME/Library/Preferences/com.googlecode.iterm2.plist $SYNCBIN/com.googlecode.iterm2.plist
+    # cp $HOME/Library/Preferences/com.googlecode.iterm2.plist $SYNCBIN/com.googlecode.iterm2.plist
+    plutil -convert xml1 -o - $HOME/Library/Preferences/com.googlecode.iterm2.plist > $SYNCBIN/com.googlecode.iterm2-xml.plist 
     echo "#########################"
     echo ""
 

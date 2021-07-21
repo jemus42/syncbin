8505db705a15f683dbe17eac0b33ccc05bc09d0c
Lukas Burk <lukas@quantenbrot.de>
2021-07-21 14:09:37 +0200

    Move  conda stuff

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -88,19 +88,3 @@ if (( $+commands[tmux] )) && [ -z $zsh_prof ] && [ -z "$TMUX" ] && [ -n "$SSH_TT
     tmux new-session -A -s ${host_short}
     # exit
 fi
-
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/Users/Lukas/Library/r-miniconda/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/Users/Lukas/Library/r-miniconda/etc/profile.d/conda.sh" ]; then
-        . "/Users/Lukas/Library/r-miniconda/etc/profile.d/conda.sh"
-    else
-        export PATH="/Users/Lukas/Library/r-miniconda/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-# <<< conda initialize <<<
-

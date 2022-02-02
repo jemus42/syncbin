658856bf4ea390593220199facff8e3e79c4ddde
Lukas Burk <lukas@quantenbrot.de>
2022-02-02 12:16:48 +0100

    k

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -56,7 +56,7 @@ alias yt-chronological="youtube-dl -i -o '%(upload_date)s -  %(title)s [%(id)s].
 # ls, the common ones I use a lot shortened for rapid fire usage
 alias l='ls -lFh'     # size,show type,human readable
 alias la='ls -lAFh'   # long list,show almost all,show type,human readable
-alias lr='ls -tRFh'   # sorted by date,recursive,show type,human readable
+# alias lr='ls -tRFh'   # sorted by date,recursive,show type,human readable
 alias lt='ls -ltFh'   # long list,sorted by date,show type,human readable
 alias ll='ls -l'      # long list
 alias ldot='ls -ld .*'

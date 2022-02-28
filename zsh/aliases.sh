427210cdc990a4b7d21f41dd5c61057270d32d19
Lukas Burk <lukas@quantenbrot.de>
2022-02-28 12:38:08 +0100

    k

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -98,10 +98,13 @@ if (($+commands[rstudio-server])); then
   alias rs="rstudio-server"
   alias rs-status="rstudio-server status"
   alias rs-active="rstudio-server active-sessions"
-  alias rs-active-count="rstudio-server active-sessions | awk '{print $5}' | awk NF | uniq -c | sort -bgr"
   alias rs-restart="sudo rstudio-server restart"
   alias rs-stop="sudo rstudio-server stop"
   alias rs-kill="sudo rstudio-server kill-session"
+
+  rs-active-count () {
+    rstudio-server active-sessions | awk '{print $5}' | awk NF | uniq -c | sort -bgr
+  }
 fi
 
 # zsh is able to auto-do some kungfoo

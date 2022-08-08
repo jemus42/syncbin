33abeae24630be24357006a7f00417505b3015f6
root <root@PPTH.fritz.box>
2022-08-08 17:40:06 +0200

    k

--- bin/update_jails.sh
+++ bin/update_jails.sh
@@ -1,18 +1,22 @@
 #! /bin/bash
 
-jails=(plex sonarr radarr syncing jackett manage webserv)
+#jails=(plex sonarr syncing manage webserv)
+jails=$(iocage list | awk '{print $4}' | awk NF | grep -v NAME)
 
-for jail in "${jails[@]}"; do
+for jail in $jails; do # "${jails[@]}"; do
 
     echo "#####################"
     echo "Updating jail $jail"
     echo "#####################"
 
-    iocage pkg $jail update
+    iocage pkg $jail update -f
     iocage pkg $jail upgrade -y
     iocage pkg $jail clean -y
     iocage pkg $jail autoremove -y
 
-    iocage restart -s $jail
+    # iocage restart -s $jail
 
 done;
+
+# iocage restart plex
+# iocage restart sonarr

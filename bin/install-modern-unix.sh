6de99ea322ff9ed1001f17853ea2ef0d41613e0f
Lukas Burk <lukas@quantenbrot.de>
2023-03-24 13:13:24 +0100

    THat was uneccessary.

--- bin/install-modern-unix.sh
+++ bin/install-modern-unix.sh
@@ -5,27 +5,30 @@ if ! [ -x "$(command -v brew)" ]; then
   exit 1
 fi
 
+# Binary name and package name differ
+echo "Checking if ripgrep is available..."
+if [ -x "$(command -v rg)" ]; then
+  echo "Found ripgrep"
+else
+  brew install ripgrep
+fi
 
-test ! -e $(command -v fd) && brew install fd
-test ! -e $(command -v sd) && brew install sd
-test ! -e $(command -v dust) && brew install dust
-test ! -e $(command -v diskus) && brew install diskus
-test ! -e $(command -v broot) && brew install broot
-test ! -e $(command -v zoxide) && brew install zoxide
-test ! -e $(command -v rg) && brew install ripgrep
-test ! -e $(command -v bat) && brew install bat
-test ! -e $(command -v exa) && brew install exa
-test ! -e $(command -v lsd) && brew install lsd
-test ! -e $(command -v delta) && brew install delta
-test ! -e $(command -v duf) && brew install duf
-test ! -e $(command -v choose) && brew install choose
-test ! -e $(command -v jq) && brew install jq
-test ! -e $(command -v yq) && brew install yq
-test ! -e $(command -v tldr) && brew install tldr
-test ! -e $(command -v cheat) && brew install cheat
-test ! -e $(command -v bottom) && brew install bottom
-test ! -e $(command -v procs) && brew install procs
-test ! -e $(command -v curlie) && brew install curlie
-test ! -e $(command -v dog) && brew install dog
-test ! -e $(command -v micro) && brew install micro
-test ! -e $(command -v thefuck) && brew install thefuck
+echo "Checking if bottom is available..."
+if [ -x "$(command -v btm)" ]; then
+  echo "Found bottom"
+else
+  brew install bottom
+fi
+
+array=( fd sd dust diskus broot zoxide bat exa lsd delta duf choose jq yq tldr cheat procs curlie dog micro thefuck )
+for i in "${array[@]}"
+do
+	echo "Checking if $i is available..."
+  if [ -x "$(command -v $i)" ]; then
+    echo "Found $i"
+  else
+    brew install $i
+  fi
+
+done
+

759b77d60a98fb9db7278b3a644dcd87658b6620
Lukas Burk <lukas@quantenbrot.de>
2023-06-20 12:57:22 +0200

    20230620125722

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -172,6 +172,28 @@ prefer-conda () {
   typeset -aU path
 }
 
+# Latex 
+cleantex () {
+	rm -rf *.out
+	rm -rf *.dvi
+	rm -rf *.log
+	rm -rf *.aux
+	rm -rf *.bbl
+	rm -rf *.blg
+	rm -rf *.ind
+	rm -rf *.idx
+	rm -rf *.ilg
+	rm -rf *.lof
+	rm -rf *.lot
+	rm -rf *.toc
+	rm -rf *.nav
+	rm -rf *.snm
+	rm -rf *.vrb
+	rm -rf *.fls
+	rm -rf *.fdb_latexmk
+	rm -rf *.synctex.gz
+	rm -rf *-concordance.tex
+}
 
 # R stuff
 upr-base () { R -e "update.packages(ask = FALSE)" }

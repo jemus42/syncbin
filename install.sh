7098b5c1a00c04b5a6751303159ca517bd9fc76d
Lukas Burk <burk@leibniz-bips.de>
2023-09-30 15:48:03 +0200

    Fix duplicate linking

--- install.sh
+++ install.sh
@@ -26,12 +26,15 @@ ln -sf $SYNCBIN/broot_conf.hjson $HOME/.config/broot/conf.hjson
 ln -sf $SYNCBIN/R/radian_profile $HOME/.radian_profile
 ln -sf $SYNCBIN/condarc $HOME/.config/conda/condarc
 ln -sf $SYNCBIN/zellij/zellij.kdl $HOME/.config/zellij/config.kdl
-ln -sf $SYNCBIN/zellij/themes $HOME/.config/zellij/themes
+
+test ! -d ~/.config/zellij/themes && ln -sf $SYNCBIN/zellij/themes $HOME/.config/zellij/themes
+test ! -d ~/.config/helix         && ln -sf $SYNCBIN/helix         $HOME/.config/helix
+
 ln -sf $SYNCBIN/lsd.conf.yml $HOME/.config/lsd/config.yaml
-ln -sf $SYNCBIN/helix $HOME/.config/helix
 ln -sf $SYNCBIN/micro/settings.json $HOME/.config/micro/settings.json
 ln -sf $SYNCBIN/micro/bindings.json $HOME/.config/micro/bindings.json
 
+# Cleanup legacy zellij conf
 test -f $HOME/.config/zellij/config.yml && rm $HOME/.config/zellij/config.yml
 
 # Install OMZSH if not present

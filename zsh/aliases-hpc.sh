6ed97f7b5297ea454ac591c5e8bb76df729fa118
Lukas Burk <lukas@quantenbrot.de>
2023-10-16 18:24:00 +0200

    20231016182400

--- zsh/aliases-hpc.sh
+++ zsh/aliases-hpc.sh
@@ -4,9 +4,9 @@ alias tmn="tmux new-session -A -s hpc"
 alias tma="tmux attach -t hpc"
 
 
-function sq () { squeue --clusters=$CLUSTERS --me -l --sort=T $@ }
-function sqr () { sq --states="R" $@ }
-function sqq () { sq --states="PD" $@ }
+function sq () { squeue --clusters=$CLUSTERS --me -l --sort=T $@; }
+function sqr () { sq --states="R" $@; }
+function sqq () { sq --states="PD" $@; }
 
 alias sqc="sq -h | wc -l"
 alias sqrc="sq -h --states=R | wc -l"
